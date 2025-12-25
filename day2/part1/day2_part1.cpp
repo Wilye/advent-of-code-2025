f60d9c5f997f3661550a357b6aa089b518024e68
Shelby Yang <shelbyyang@Mac-11405.lan>
2025-12-25 12:19:09 -0800

    finished day 2 part 1

--- day2/part1/day2_part1.cpp
+++ day2/part1/day2_part1.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdint>
 
-void processRange(std::string& range) {
-    std::cout << "range: " << range << std::endl;
+int countDigits(uint64_t num) {
+    if (num == 0) return 1;
+
+    int count = 0;
+    
+    while (num > 0) {
+        num /=  10;
+        ++count;
+    }
+
+    return count;
+}
+
+uint64_t processRange(std::string& range) {
+    //std::cout << "range: " << range << std::endl;
+
+    uint64_t sum_of_invalid = 0;
+
+    size_t pos = range.find('-');
+    if (pos != std::string::npos) {
+        uint64_t before = std::stoull(range.substr(0, pos));
+        uint64_t after = std::stoull(range.substr(pos + 1));
+
+        uint64_t val = before;
+        while (val <= after) {
+            int num_digits = countDigits(val);
+            if (num_digits % 2 == 0) {
+                int mid_index = num_digits / 2;
+                std::string val_string = std::to_string(val);
+                int first_half = std::stoi(val_string.substr(0, mid_index));
+                int second_half = std::stoi(val_string.substr(mid_index));
+                if (first_half == second_half) {
+                    // invalid
+                    sum_of_invalid += val;
+                }
+            }
+            ++val;
+        }
+    }
+
+    return sum_of_invalid;
 }
 
 int main(int argc, char* argv[]) {
@@ -27,7 +67,10 @@ int main(int argc, char* argv[]) {
     std::istringstream iss(line);
 
     std::string range;
+    uint64_t sum_of_invalid = 0;
     while(std::getline(iss, range, ',')) {
-        processRange(range);
+        sum_of_invalid += processRange(range);
     }
+
+    std::cout << "Sum of invalid IDs: " << sum_of_invalid << std::endl;
 }
